f89a9df1722a9e767896f16dd4026842ae25d665
Jack Coates <68520860+JackCoates1@users.noreply.github.com>
2026-05-22 11:16:59 +0100

    Add Nav and Timeline to page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,17 +1,23 @@
+import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import Timeline from "@/components/Timeline";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
-      <Hero />
+      <Nav />
+      <div id="home">
+        <Hero />
+      </div>
       <About />
       <Skills />
       <Projects />
+      <Timeline />
       <Contact />
       <Footer />
     </div>
